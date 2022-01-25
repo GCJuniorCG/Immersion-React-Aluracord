bb1720069f035feb68e377270c05665497406523
Gilberto Camargo Jr <gilbertocamargojr79@gmail.com>
2022-01-25 13:27:12 -0300

    matrix1

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json'
-
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
 
 function Titulo(props) {
     const Tag = props.tag || 'h1';
@@ -66,11 +39,15 @@ function Titulo(props) {
   export default HomePage*/
 
   export default function PaginaInicial() {
-    const username = 'GCJuniorCG';
+  //  const username = 'GCJuniorCG';
   
+    const [username, setUsername] = React.useState('GCJuniorCG');
+    const roteamento = useRouter();
+
+
+
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -97,6 +74,12 @@ function Titulo(props) {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function (infosDoEvento){
+                infosDoEvento.preventDefault();
+                roteamento.push('./chat');
+                //window.location.href = ('./chat');
+              }
+            }
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -106,8 +89,16 @@ function Titulo(props) {
               <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                 {appConfig.name}
               </Text>
-  
+
               <TextField
+                 value={username}
+                 onChange={function (event) {
+                     console.log('usuario digitou', event.target.value);
+                     //onde esta o valor
+                     const valor = event.target.value;
+                     //trocar valor variavel
+                     setUsername(valor);
+                 }}
                 fullWidth
                 textFieldColors={{
                   neutral: {
